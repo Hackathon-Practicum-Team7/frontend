c651efe464edac450471d1fbf839931230125bbc
Elana Tollu <elana.tollu@yandex.ru>
2023-10-24 00:08:25 +0300

    add ProfilePage

--- src/components/routes-component/routes-component.tsx
+++ src/components/routes-component/routes-component.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { ReactElement } from 'react'
 import { HomePage } from "../../pages/home"
+import { ProfilePage } from '../../pages/profile';
 
 export default function RoutesComponent(): ReactElement {
   return (
     <Routes>
       <Route path='/login' element={<></>} />
       <Route path='/' element={<HomePage />} />
+      <Route path='/profile/:id' element={<ProfilePage />} />
       <Route path='*' element={<></>} />
   </Routes>
 );
