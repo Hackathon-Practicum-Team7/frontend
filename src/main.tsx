d6cccf548ecae27e7ad502c0f0e625a985eea1b8
Elana Tollu <elana.tollu@yandex.ru>
2023-10-25 01:35:58 +0300

    add Footer

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './components/app/app.tsx'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Footer } from './components/footer/footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
     <App />
-    <Footer />
   </React.StrictMode>,
 )
