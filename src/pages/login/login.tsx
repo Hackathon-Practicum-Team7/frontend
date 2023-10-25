92b12f08b4cf06200070fbbb36920923a7565190
dariarus <rusanova_dv@mail.ru>
2023-10-26 00:13:37 +0300

    refactor/feature: refactor MUI-component Input into TextField. Start setting validation per react-hook-form + yup

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,87 +1,37 @@
 import React, {FunctionComponent} from 'react';
-
 import {
   FormControl,
-  FormControlState,
   Input,
-  InputProps, Link,
-  makeStyles,
+  Link,
   TextField,
   Typography
 } from '@mui/material';
 
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+
 import loginStyles from './login.module.css';
+
+import {authValidationSchema, customLoginStyles} from '../../utils/constants';
 
 import {CustomButton} from '../../components/custom-button/custom-button';
 
-export const Login: FunctionComponent = () => {
-  const customLoginStyles = {
-    text: {
-      fontStyle: 'normal',
-    },
-    'text_heading': {
-      fontFamily: `'YS-Display', ` + `'Arial', ` + 'sans-serif',
-      marginBottom: '4px',
-      fontWeight: '500',
-      fontSize: '24px',
-      lineHeight: '32px',
-      color: '#1a1b22',
-    },
-    'text_subheading': {
-      fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
-      fontWeight: '400',
-      fontSize: '18px',
-      lineHeight: '24px',
-      color: '#1a1b22',
-    },
-    'text_subtitle': {
-      fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
-      fontWeight: '400',
-      fontSize: '14px',
-      lineHeight: '20px',
-      color: '#797981',
-    },
-    'text_link': {
-      marginBottom: '24px',
-      fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
-      fontWeight: '400',
-      fontSize: '14px',
-      lineHeight: '20px',
-    },
-    input: {
-      boxSizing: 'border-box',
-      padding: '12px 15px',
-      border: '1px solid #797981',
-      borderRadius: '4px',
-      fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
-      fontStyle: 'normal',
-      fontWeight: '400',
-      fontSize: '14px',
-      lineHeight: '20px',
-      color: '#1a1b22',
-      '.MuiInputBase-input.MuiInput-input': {
-        padding: '0',
-      },
-      '&.Mui-focused': {
-        border: '2px solid #5a9Bff',
-      },
-      '&.MuiInputBase-root:hover:before': {
-        borderBottom: 'none',
-      },
+import {TAuthForm} from '../../utils/types';
 
-      ':placeholder': {
-        color: '#797981',
-      },
-      '::before': {
-        content: 'none',
-        border: 'none',
-      },
-      '::after': {
-        content: 'none',
-        border: 'none',
-      },
+export const Login: FunctionComponent = () => {
+  const form = useForm<TAuthForm>({
+    defaultValues: {
+      email: '',
+      password: ''
     },
-  }
+    resolver: yupResolver(authValidationSchema),
+  });
+  const {register, handleSubmit, formState: {errors}} = form;
+
+  const submitAuthForm = (data: TAuthForm | undefined) => {
+    console.log(errors);
+    console.log({data});
+  };
 
   return (
     <section className={loginStyles['login-page']}>
@@ -92,27 +42,56 @@ export const Login: FunctionComponent = () => {
         <Typography variant="h2" align="center" sx={[customLoginStyles.text, customLoginStyles['text_subheading']]}>
           для работодателей
         </Typography>
-        <form>
+        <form onSubmit={handleSubmit(submitAuthForm)} noValidate>
           <div className={loginStyles['login-page__inputs-wrap']}>
             <Typography variant="subtitle1" align="center"
                         sx={[customLoginStyles.text, customLoginStyles['text_subtitle']]}>
               Войти в аккаунт
             </Typography>
-            <FormControl defaultValue="" required>
-              <Input placeholder="Почта" fullWidth sx={customLoginStyles.input}/>
-            </FormControl>
-            <FormControl defaultValue="" required>
-              <Input type="password" placeholder="Пароль" fullWidth sx={customLoginStyles.input}/>
-            </FormControl>
+            <TextField variant="standard"
+                       name="email"
+                       type="email"
+                       {...register("email")}
+                       placeholder="Почта"
+                       fullWidth
+                       error={!!errors.email}
+                       sx={customLoginStyles.outline}
+                       InputProps={{disableUnderline: true}}
+                       inputProps={{sx: customLoginStyles.input}}
+            />
+            {
+              errors.email &&
+              <p>{errors.email?.message}</p>
+            }
+            <TextField variant="standard"
+                       name="password"
+                       type="password"
+                       {...register("password")}
+                       placeholder="Пароль"
+                       fullWidth
+                       error={!!errors.password}
+                       sx={customLoginStyles.outline}
+                       InputProps={{disableUnderline: true}}
+                       inputProps={{sx: customLoginStyles.input}}
+            />
           </div>
-          <Typography paragraph>
-            <Link href="#" color="#1d6bf3" underline="hover" sx={customLoginStyles['text_link']}>
+          <Typography paragraph sx={[customLoginStyles.text, customLoginStyles['text_subtitle']]}>
+            <Link href="#" color="#1d6bf3" underline="hover" sx={[customLoginStyles.text, customLoginStyles['text_form-link']]}>
               Не помню пароль
             </Link>
           </Typography>
-          <CustomButton customType="customContained" width="100%">Войти</CustomButton>
+          <CustomButton customType="customContained" width="100%"
+                        onClick={handleSubmit(submitAuthForm)}>Войти</CustomButton>
         </form>
       </div>
+      <Typography paragraph sx={[customLoginStyles.text, customLoginStyles['text_secondary']]}>
+        Новый пользователь?
+        <Link href="#"
+              underline="hover"
+              sx={[customLoginStyles.text, customLoginStyles['text_secondary'], customLoginStyles['text_link']]}>
+          Зарегистрироваться
+        </Link>
+      </Typography>
     </section>
   )
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material";
+import * as yup from 'yup';
 
 declare module '@mui/material/Button' {
   interface ButtonPropsVariantOverrides {
@@ -47,4 +48,91 @@ export const theme = createTheme({
       ],
     },
   },
+});
+
+
+export const customLoginStyles = {
+  text: {
+    fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
+    fontStyle: 'normal',
+  },
+  'text_heading': {
+    fontFamily: `'YS-Display', ` + `'Arial', ` + 'sans-serif',
+    marginBottom: '4px',
+    fontWeight: '500',
+    fontSize: '24px',
+    lineHeight: '32px',
+    color: '#1a1b22',
+  },
+  'text_subheading': {
+    fontWeight: '400',
+    fontSize: '18px',
+    lineHeight: '24px',
+    color: '#1a1b22',
+  },
+  'text_subtitle': {
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '20px',
+    color: '#797981',
+  },
+  'text_form-link': {
+    marginBottom: '24px',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '20px',
+  },
+  'text_secondary': {
+    marginTop: '20px',
+    fontSize: '16px',
+    lineHeight: '20px',
+    color: '#b5b5b7',
+  },
+  'text_link': {
+    marginLeft: '5px',
+    color: '#fff',
+  },
+  input: {
+    border: 'none',
+    fontFamily: `'YS-Text', ` + `'Arial', ` + 'sans-serif',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '20px',
+    '&.MuiInput-input': {
+      padding: '12px 15px',
+    },
+    ':placeholder': {
+      color: '#797981',
+    },
+    '::before': {
+      content: 'none',
+      border: 'none',
+    },
+    '::after': {
+      content: 'none',
+      border: 'none',
+    },
+  },
+  'input_errored': {
+    border: '2px solid #ff0200',
+  },
+  outline: {
+    border: '1px solid #797981',
+    borderRadius: '4px',
+    ':has(> .Mui-focused)': {
+      border: '2px solid #5a9bff',
+    }
+  }
+}
+
+export const authValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Введите корректный адрес электронной почты')
+    .required('Это поле обязательно'),
+  password: yup
+    .string()
+    .min(6, 'Пароль должен содержать не менее 6 символов')
+    .required('Это поле обязательно')
 });
