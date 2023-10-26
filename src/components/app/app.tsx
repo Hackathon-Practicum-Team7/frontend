e5a3f7579e102ba3d6d0fef43c5a212f9bbf1feb
Tatiana Andreeva <snurnisyan@gmail.com>
2023-10-26 15:09:24 +0300

    fix: fix header & footer postions + minor style changes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,12 +2,18 @@ import './app.css'
 import { BrowserRouter } from "react-router-dom";
 import RoutesComponent from "../routes-component/routes-component";
 import {ReactElement} from "react";
+import {Header} from "../header/header";
+import {Footer} from "../footer/footer";
 
 function App(): ReactElement {
 
   return (
     <BrowserRouter>
-      <RoutesComponent />
+      <Header />
+      <main style={{ width: '1440px' }}>
+        <RoutesComponent />
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
