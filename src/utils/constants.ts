8d99fa9cc80aa450ae0873540751d0f101fd0076
Tatiana Andreeva <snurnisyan@gmail.com>
2023-10-26 21:11:49 +0300

    fix: minor table style changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -294,3 +294,36 @@ export const authValidationSchema = yup.object().shape({
     .min(6, 'Пароль должен содержать не менее 6 символов')
     .required('Это поле обязательно')
 });
+
+export const student = {
+    "name": "Ирина",
+    "surname": "Иванова",
+    "profession": "Web-разработчик",
+    "grade": "middle",
+    "city": "Санкт-Петербург",
+    "started_working": "2023-02-02",
+    "about": "Я дизайнер",
+    "avatar": "student_35ce3300-9c12-40b6-93ce-c03b7548200d/avatars/maxresdefault.jpg",
+    "resume": "student_35ce3300-9c12-40b6-93ce-c03b7548200d/resumes/hakaton.pdf",
+    "is_looking_for_job": true,
+    "has_portfolio": true,
+    "employment_types": [
+      "Полная занятось",
+      "Частичная занятость",
+      "Проектная работа",
+      "Стажировка"
+    ],
+    "working_condition": [
+      "Офис",
+      "Гибрид",
+      "Удаленка"
+    ],
+    "contact": {
+    "id": 2,
+      "email": "student2@example.com",
+      "phone": "+79998887744",
+      "telegram": "https://t.me/alena",
+      "portfolio": "https://github.com/alena",
+      "whatsapp": "+79998887744"
+    },
+}
