3aa026a43e2aed2fd110d8c9a1f3928d28863e45
Elana Tollu <elana.tollu@yandex.ru>
2023-10-26 04:15:02 +0300

    add CardEducation

---
+++ src/components/cardEducation/cardEducation.tsx
@@ -0,0 +1,76 @@
+import { Card, CardContent, CardHeader, ThemeProvider, createTheme } from '@mui/material';
+import React from 'react';
+import styles from './cardEducation.module.css';
+
+export const CardEducation: React.FC = () => {
+
+  const theme = createTheme({
+    components: {
+      MuiCard: {
+        styleOverrides: {
+          root: {
+            borderBox: 'none',
+            borderRadius: '0',
+            boxShadow: 'none'
+          },
+        },
+      },
+      MuiCardHeader: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#FFF',
+            color: '#000',
+            textTransform: 'none',
+            fontSize: '24px',
+            fontWeight: '500',
+            paddingTop: '0',
+            paddingLeft: '0',
+            height: '60px'
+          },
+        },
+      },
+      MuiCardContent: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#F1F6FF',
+            color: '#000',
+            textTransform: 'none',
+            minHeight: '396px',
+            padding: '0'
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Card>
+        <CardHeader title="Образование" />
+        <CardContent>
+          <div className={styles.content}>
+
+            <div className={styles.item}>
+              <p className={styles.time}>2023</p>
+              <p className={styles.title}>Продуктовый дизайнер</p>
+              <p className={styles.company}>Менторство в Helper, Студия MAX</p>
+            </div>
+
+            <div className={styles.item}>
+              <p className={styles.time}>2021</p>
+              <p className={styles.title}>UI  дизайнер</p>
+              <p className={styles.company}>Пятигорский техникум экономики и инновационных технологий</p>
+            </div>
+
+            <div className={styles.item}>
+              <p className={styles.time}>2013 - 2017</p>
+              <p className={styles.title}>Дизайнер</p>
+              <p className={styles.company}>Рязанский государственный радиотехнический университет, Рязань</p>
+            </div>
+
+          </div>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
+  )
+}
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,55 +1,13 @@
-import { Card, CardContent, CardHeader, ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './profile.module.css';
 import { CardAbout } from '../components/cardAbout/cardAbout';
 import { CardSkills } from '../components/cardSkills/cardSkills';
 import { CardExperience } from '../components/cardExperience/cardExperience';
+import { CardEducation } from '../components/cardEducation/cardEducation';
 
 export const ProfilePage: React.FC = () => {
   const { id } = useParams();
-
-  const theme = createTheme({
-    components: {
-      MuiCard: {
-        styleOverrides: {
-          root: {
-            borderBox: 'none',
-            borderRadius: '0',
-            boxShadow: 'none'
-          },
-        },
-      },
-      MuiCardHeader: {
-        styleOverrides: {
-          root: {
-            backgroundColor: '#FFF',
-            color: '#000',
-            textTransform: 'none',
-            fontSize: '24px',
-            fontWeight: '500',
-            paddingTop: '0',
-            paddingLeft: '0',
-            height: '60px'
-          },
-        },
-      },
-      MuiCardContent: {
-        styleOverrides: {
-          root: {
-            backgroundColor: '#F1F6FF',
-            color: '#000',
-            textTransform: 'none',
-            fontSize: '16px',
-            lineHeight: '20px',
-            fontWeight: '400',
-            minHeightheight: '128px',
-            padding: '0'
-          },
-        },
-      },
-    },
-  });
 
   return (
     <div>
@@ -64,23 +22,7 @@ export const ProfilePage: React.FC = () => {
 
       <CardExperience></CardExperience>
 
-      <ThemeProvider theme={theme}>
-
-      <Card>
-        <CardHeader title="Образование" />
-        <CardContent>
-6 лет
-2023
-Продуктовый дизайнер
-Менторство в Helper, Студия MAX
-2021
-UI  дизайнер
-Пятигорский техникум экономики и инновационных технологий
-2013 - 2017
-Дизайнер
-Рязанский государственный радиотехнический университет, Рязань</CardContent>
-      </Card>
-     </ThemeProvider>
+      <CardEducation></CardEducation>
 
      </div>
 
