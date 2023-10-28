0c11c0ded49f227ddde45f98795fa76454571a26
Tatiana Andreeva <snurnisyan@gmail.com>
2023-10-28 16:11:29 +0300

    feat: add table sorting

--- src/components/enhanced-table-head/enhanced-table-head.tsx
+++ src/components/enhanced-table-head/enhanced-table-head.tsx
@@ -11,6 +11,7 @@ import {headCells, newHeadCells} from "../../utils/constants/constants";
 import checkboxIcon from "../../images/checkbox.svg";
 import styles from "../table/table.module.css";
 import inactiveCheckboxIcon from "../../images/checkbox-not-active.svg";
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
 
 interface EnhancedTableProps {
   numSelected: number;
@@ -66,18 +67,23 @@ export default function EnhancedTableHead(props: EnhancedTableProps) {
             padding={'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+            {headCell.sortable ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+                IconComponent={ExpandMoreRoundedIcon}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            ) : (
+              headCell.label
+            )}
           </TableCell>
         ))}
       </TableRow>
